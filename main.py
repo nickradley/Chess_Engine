538cdd65bf0afedddc4877c38d16af5f5c313400
Nicholas Radley <nick@Nicholass-MacBook-Pro.local>
2022-04-22 11:43:52 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#DeezNutzInc Chess Engine
+from tkinter import *
+
+window=Tk()
+window.title('Chess Engine')
+window.geometry('500x500+500+400')
+window.configure(bg='black')
+lbl=Label(window, text="Press the button below me", bg='black', fg='gray', font=("Helvetica", 16))
+lbl.place(x=170, y=180)
+btn=Button(window, text="You are Retarded", bg='black', fg='blue')
+btn.place(x=200, y=200)
+window.mainloop()
+
+
