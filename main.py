10f8f2ad3e6d12630ab0f94c03c308b60b4c56ae
alecmw24 <alecmartinwalter@gmail.com>
2022-04-27 16:08:32 -0700

    fixed error with placing pieces on board and added functionality for FEN command line arg

--- main.py
+++ main.py
@@ -1,45 +1,66 @@
-#DeezNutzInc Chess Engine
+# DeezNutzInc Chess Engine
 from tkinter import *
-from PIL import ImageTk,Image
+from PIL import Image, ImageTk
+
 import pathlib
+import sys
 
 WINDOW_HEIGHT = 1000
 WINDOW_WIDTH = 1200
 SQUARESIZE = 90
 BOARD_HORIZ_OFFSET = 100
 BOARD_VERT_OFFSET = 20
-#the following function places pieces on the board according to a passed FEN string. For now, we consider
+
+
+# the following function places pieces on the board according to a passed FEN string. For now, we consider
 # only the part of the FEN which gives the location of the pieces, without considering castling rights,
 # en passant, move number etc.
 def FEN_to_array(FEN):
-    fen_ranks=FEN.split("/")
-    board=[[0]*8for _ in range(8)]
-    row=0
-    col=0
-
+    fen_ranks = FEN.split("/")
+    board = [[0] * 8 for _ in range(8)]
+    row = 0
+    col = 0
     for rank in fen_ranks:
         for element in rank:
-            if (element.isdigit()):
-                col+=int(element)
+            if element.isdigit():
+                col += int(element)
             else:
-                board[row][col]=element
-                col+=1
-        row+=1
-        col=0
+                board[row][col] = element
+                col += 1
+        row += 1
+        col = 0
     return board
 
-def init_window(window, canvas):
+
+# The following function initializes the window and places pieces based on the inputted
+# fen string param
+def init_window(window, canvas, fen):
     window.title('Chess Engine')
     window.geometry(str(WINDOW_WIDTH) + "x" + str(WINDOW_HEIGHT))
     window.configure(bg="grey")
     canvas.pack()
+    # generate board grid
     for file in range(8):
         for rank in range(8):
-            square = canvas.create_rectangle(BOARD_HORIZ_OFFSET + (SQUARESIZE * rank),
-                                             BOARD_VERT_OFFSET + (SQUARESIZE * file),
-                                             BOARD_HORIZ_OFFSET + (SQUARESIZE * (rank + 1)),
-                                             BOARD_VERT_OFFSET + (SQUARESIZE * (file + 1)),
+            x1 = BOARD_HORIZ_OFFSET + (SQUARESIZE * rank)
+            y1 = BOARD_VERT_OFFSET + (SQUARESIZE * file)
+            x2 = BOARD_HORIZ_OFFSET + (SQUARESIZE * (rank + 1))
+            y2 = BOARD_VERT_OFFSET + (SQUARESIZE * (file + 1))
+            square = canvas.create_rectangle(x1, y1, x2, y2,
                                              fill='#302311' if (file + rank) % 2 != 0 else '#cfaf86')
+    # import pieces
+    global black_pawn
+    global black_bishop
+    global black_knight
+    global black_rook
+    global black_king
+    global black_queen
+    global white_pawn
+    global white_bishop
+    global white_knight
+    global white_rook
+    global white_king
+    global white_queen
     black_pawn = ImageTk.PhotoImage(Image.open("images/Black_Pawn.png"))
     black_bishop = ImageTk.PhotoImage(Image.open("images/Black_Bishop.png"))
     black_knight = ImageTk.PhotoImage(Image.open("images/Black_Knight.png"))
@@ -52,7 +73,8 @@ def init_window(window, canvas):
     white_rook = ImageTk.PhotoImage(Image.open("images/White_Rook.png"))
     white_king = ImageTk.PhotoImage(Image.open("images/White_King.png"))
     white_queen = ImageTk.PhotoImage(Image.open("images/White_Queen.png"))
-    board = FEN_to_array("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
+    # arrange pieces according to fen param
+    board = FEN_to_array(fen)
     switcher = {
         "p": black_pawn,
         "b": black_bishop,
@@ -69,12 +91,24 @@ def init_window(window, canvas):
     }
     for file in range(8):
         for rank in range(8):
-            if (board[rank][file] != 0):
-                id=canvas.create_image(BOARD_HORIZ_OFFSET + (SQUARESIZE * rank),
-                                    BOARD_VERT_OFFSET + (SQUARESIZE * file),
-                                    anchor=NW, image=switcher[board[rank][file]])
+            if board[file][rank] != 0:
+                canvas.create_image(BOARD_HORIZ_OFFSET + (SQUARESIZE * rank) + SQUARESIZE * .15,
+                                    BOARD_VERT_OFFSET + (SQUARESIZE * file) + SQUARESIZE * .15,
+                                    anchor=NW, image=switcher[board[file][rank]])
 
-window=Tk()
-canvas = Canvas(window, height=WINDOW_HEIGHT, width=WINDOW_WIDTH)
-init_window(window, canvas)
-window.mainloop()
+
+def main():
+    # Fetch fen command line arg, if any
+    argv = sys.argv[1:]
+    if len(argv) == 0:
+        fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+    else:
+        fen = argv[0]
+    # Create and init window
+    window = Tk()
+    canvas = Canvas(window, height=WINDOW_HEIGHT, width=WINDOW_WIDTH, bg='lightgrey')
+    init_window(window, canvas, fen)
+    window.mainloop()
+
+
+main()
